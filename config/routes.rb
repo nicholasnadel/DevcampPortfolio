6f005850ffe64aa615dc1bc9672c58359cf66540
nicholasnadel <nicholasnadel@gmail.com>
2018-05-30 16:39:15 -0700

    custom scope for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :portfolios, except: [:show]
+  get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-
   resources :blogs do
     member do
       get :toggle_status
