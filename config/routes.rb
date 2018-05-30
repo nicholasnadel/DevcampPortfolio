83e933dee932172be246fa3698f3eceb8c96a0ae
nicholasnadel <nicholasnadel@gmail.com>
2018-05-30 10:48:24 -0700

    add friendly routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
