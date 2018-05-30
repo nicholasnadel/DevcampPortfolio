acfdfe2ece827c093d431dc3ba54b62d36756b08
nicholasnadel <nicholasnadel@gmail.com>
2018-05-29 22:55:43 -0700

    new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,7 +3,21 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def new
+    @portfolio_item = Portfolio.new
+  end
 
+ def create
+    @portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to @portfolios_path, notice: 'Portfolio item successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 
   private
 
