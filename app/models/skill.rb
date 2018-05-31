693bb72a800e59af3a796cc1570d24430c996452
nicholasnadel <nicholasnadel@gmail.com>
2018-05-31 13:28:52 -0700

    resolve local conflict

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,15 +1,11 @@
 class Skill < ApplicationRecord
-<<<<<<< HEAD
   include Placeholder
   validates_presence_of :title, :percent_utilized
 
   def set_defaults
     self.badge ||= Placeholder.image_generator(height: '250', width: '250')
   end
-
-  validates_presence_of :title, :precent_utilized
-
-=======
+  
   validates_presence_of :title, :percent_utilized
->>>>>>> authentication
+
 end
