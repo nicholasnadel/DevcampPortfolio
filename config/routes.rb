87f12f9529c3c2a9a91d9f80d018328821d2eac1
nicholasnadel <nicholasnadel@gmail.com>
2018-05-31 01:59:44 -0700

    add session tracker for dynamic source links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
   get 'pages/home'
 
   get 'pages/about'
@@ -8,9 +11,8 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  resources :portfolios, except: [:show]
-  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
