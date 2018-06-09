e2474bb84e70385b2b44498089d44a7ee7c15170
nicholasnadel <nicholasnadel@gmail.com>
2018-06-09 01:38:30 -0700

    implement authorization engine for portfolio items & blogs

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -7,11 +7,7 @@ class Skill < ApplicationRecord
   def set_defaults
     self.badge ||= Placeholder.image_generator(height: '250', width: '250')
   end
-<<<<<<< HEAD
-  
+
   validates_presence_of :title, :percent_utilized
 
 end
-=======
-end
->>>>>>> c8b942fd744cc3270676589f60bb753a01acbbbc
