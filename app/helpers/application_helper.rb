b50120c4d07aad9a9a08555a33b4b13e15566ea8
nicholasnadel <nicholasnadel@gmail.com>
2018-06-09 20:54:05 -0700

    update styles for master layout file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
   def login_helper style
     if current_user.is_a?(User)
-      (link_to "Register", new_user_registration_path, class: style) +
-        "<br>".html_safe +
-        (link_to "Login", new_user_session_path, class: style)
-    else
       link_to "Logout", destroy_user_session_path, method: :delete, class: style
+
+    else
+      (link_to "Register", new_user_registration_path, class: style) +
+      " ".html_safe +
+      (link_to "Login", new_user_session_path, class: style)
     end
   end
 
@@ -16,4 +17,7 @@ module ApplicationHelper
     end
   end
 
+  def copyright_generator
+    DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
+  end
 end
